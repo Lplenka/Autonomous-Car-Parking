30ffeae0526ea8cc4f28828669443b3358d62ea2
Lalu Prasad Lenka <laluprasad.lenka95@gmail.com>
2022-03-21 01:13:42 +0000

    Updated SAC+HER as per stablebaselines3

--- agents/reinforcement_learning/sac_her_agent.py
+++ agents/reinforcement_learning/sac_her_agent.py
@@ -2,27 +2,47 @@ import gym
 import highway_env
 import numpy as np
 
-from stable_baselines import HER, SAC, DDPG, TD3
-from stable_baselines.ddpg import NormalActionNoise
+from stable_baselines3 import HerReplayBuffer, SAC, DDPG, TD3
+# from stable_baselines3.ddpg import NormalActionNoise
 
 env = gym.make("parking-v0")
 
 # Create 4 artificial transitions per real transition
 n_sampled_goal = 4
 
-# SAC hyperparams:
-model = HER('MlpPolicy', env, SAC, n_sampled_goal=n_sampled_goal,
-            goal_selection_strategy='future',
-            verbose=1, buffer_size=int(1e6),
-            learning_rate=1e-3,
-            gamma=0.95, batch_size=256,
-            policy_kwargs=dict(layers=[256, 256, 256]))
+model_class = SAC  # works also with DQN, DDPG and TD3
+N_BITS = 15
 
-model.learn(int(2e5))
-model.save('her_sac_highway')
+# Available strategies (cf paper): future, final, episode
+goal_selection_strategy = 'future' # equivalent to GoalSelectionStrategy.FUTURE
 
-# Load saved model
-model = HER.load('her_sac_highway', env=env)
+# If True the HER transitions will get sampled online
+online_sampling = True
+# Time limit for the episodes
+max_episode_length = N_BITS
+
+# Initialize the model
+model = model_class(
+    "MultiInputPolicy",
+    env,
+    replay_buffer_class=HerReplayBuffer,
+    # Parameters for HER
+    replay_buffer_kwargs=dict(
+        n_sampled_goal=n_sampled_goal,
+        goal_selection_strategy=goal_selection_strategy,
+        online_sampling=online_sampling,
+        max_episode_length=max_episode_length,
+    ),
+    verbose=1,
+)
+
+# Train the model
+model.learn(1000)
+
+model.save("./her_car_env")
+# Because it needs access to `env.compute_reward()`
+# HER must be loaded with the env
+model = model_class.load('./her_car_env', env=env)
 
 obs = env.reset()
 
